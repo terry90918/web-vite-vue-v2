41b7275f78cd9a23f75cd228006f0fc4ff7dd944
terry90918 <zxtw17985321@gmail.com>
2024-01-14 23:14:33 +0800

    feat: lib

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.js'),
+      name: 'MyLib',
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
