a165b008eeecc1b0c40de4cac2dfcf9ca0564914
terry90918 <zxtw17985321@gmail.com>
2024-01-14 23:31:11 +0800

    feat: use web-vite-vue-v2 package

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Foo />
+    <Bar />
   </div>
 </template>
+
+<script>
+import { Foo, Bar } from 'web-vite-vue-v2'
+
+export default {
+  components: {
+    Foo,
+    Bar
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
